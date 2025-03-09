8548fc3c1fbce5705c02b331e4f0f574c3fbeef5
dev-anannya <anannya614@gmail.com>
2025-03-09 15:07:34 +0530

    Minor changes regarding webpage titles

--- category.php
+++ category.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Porfyro Boutique - Category</title>
+    <title>nya's collections</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
@@ -66,7 +66,7 @@
 
     <!-- Footer -->
     <footer>
-        <p>&copy; 2025 Porfyro Boutique. All Rights Reserved.</p>
+        <p>&copy; 2025 nya's. All Rights Reserved.</p>
     </footer>
 
     <script src="script.js"></script>
